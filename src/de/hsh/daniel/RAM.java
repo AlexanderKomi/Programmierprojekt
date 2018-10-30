5d8c265426716ce582bca9f0a51c2cffc4f8c2d2
Alex <alexander.komischke@stud.uni-hannover.de>
2018-10-30 15:37:27 +0100

    Added RAM

--- src/de/hsh/daniel/RAM.java
+++ src/de/hsh/daniel/RAM.java
@@ -1,4 +1,17 @@
 package de.hsh.daniel;
 
-public class RAM {
+import de.hsh.alexander.engine.game.Game;
+import javafx.scene.layout.Pane;
+
+import java.util.Observer;
+
+public class RAM extends Game {
+    protected RAM( Observer o, String name ) {
+        super( o, name );
+    }
+
+    @Override
+    public Pane initGameContentWindow( Observer observer ) {
+        return null;
+    }
 }
