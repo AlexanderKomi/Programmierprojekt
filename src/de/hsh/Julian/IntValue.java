4db442c4c0ca6253249005c0dcc1e9ff40cd745e
JuSen88 <j.sender88@gmail.com>
2018-11-01 11:26:48 +0100

    Added Template to start working / learn how to JavaFX

---
+++ src/de/hsh/Julian/IntValue.java
@@ -0,0 +1,11 @@
+package de.hsh.Julian;
+
+public class IntValue
+{
+    public int value;
+
+    public IntValue(int i)
+    {
+        value = i;
+    }
+}
---
+++ src/de/hsh/Julian/LongValue.java
@@ -0,0 +1,10 @@
+package de.hsh.Julian;
+public class LongValue
+{
+    public long value;
+
+    public LongValue(long i)
+    {
+        value = i;
+    }
+}
