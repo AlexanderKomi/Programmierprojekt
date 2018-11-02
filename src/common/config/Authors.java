e8194531669d52f3e2d878df348cdd1977f90b1e
Alex <alexander.komischke@stud.uni-hannover.de>
2018-11-02 16:57:43 +0100

    Removed hard Strings

---
+++ src/common/config/Authors.java
@@ -0,0 +1,14 @@
+package common.config;
+
+public class Authors {
+
+    public static final String[] authorNames = new String[] {
+            "Alexander Komischke",
+            "Dennis Sellemann",
+            "Julian Sender",
+            "Daniel Diele",
+            "Kevin",
+            "Amir-Hossein Ebrahimzadeh"
+    };
+
+}
