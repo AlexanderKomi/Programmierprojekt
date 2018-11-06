b18d276eb269d741cc97f6e390ae49548814af96
Dennis Sellemann <dsellemann@gmail.com>
2018-11-06 11:07:55 +0100

    uploaded Dennis Gui: fxml and basic controller

---
+++ src/de/hsh/dennis/controller/ddd_MainMenue_controller.java
@@ -0,0 +1,35 @@
+package de.hsh.dennis.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class ddd_MainMenue_controller {
+
+    @FXML
+    private HBox hbox_1;
+
+    @FXML
+    private VBox vbox_1;
+
+    @FXML
+    private ImageView pic_header;
+
+    @FXML
+    private Button b_play;
+
+    @FXML
+    private Button b_tutorial;
+
+    @FXML
+    private Button b_exit;
+
+    @FXML
+    void button_clicked(ActionEvent event) {
+
+    }
+
+}
