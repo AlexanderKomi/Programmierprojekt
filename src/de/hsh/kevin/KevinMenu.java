8d600d6feb7d1727218a5c6bd546f99a3cd93bf4
Kevin Jeske <jeske.kevin@web.de>
2018-11-12 21:20:30 +0100

    Versuch FXML zu laden

--- src/de/hsh/kevin/KevinMenu.java
+++ src/de/hsh/kevin/KevinMenu.java
@@ -1,6 +1,9 @@
 package de.hsh.kevin;
 
 import de.hsh.alexander.engine.game.GameMenu;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 
 import java.net.URL;
@@ -8,17 +11,63 @@ import java.util.Observer;
 import java.util.ResourceBundle;
 
 public class KevinMenu extends GameMenu {
+    
+    public static final String fxml = "res/TIMenu.fxml";
+    
+    @FXML
+    public Button btn_start;
+    
+    @FXML
+    public Button btn_sound;
+    
+    @FXML
+    public Button btn_schwierigkeit;
+    
+    @FXML
+    public Button btn_sammlung;
+    
+    @FXML
+    public Button btn_exit;
+    
+    
+    
     public KevinMenu( Observer observer ) {
         super( observer );
+        
     }
 
     @Override
     protected Pane initMenuPane() {
-        return null;
+        return new Pane();
     }
 
     @Override
     public void initialize( URL location, ResourceBundle resources ) {
+
+    }
+
+    @FXML
+    void difficultyPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sammlungPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void soundPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void startGamePressed(ActionEvent event) {
 
     }
 }
