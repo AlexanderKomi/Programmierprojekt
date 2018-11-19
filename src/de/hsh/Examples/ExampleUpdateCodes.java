3b554fdf348c1d321f2ec9d4e9b15e10cbfd4db6
Dennis Sellemann <dsellemann@gmail.com>
2018-11-19 12:36:37 +0100

    Example: Updatecodes und optimierung

---
+++ src/de/hsh/Examples/ExampleUpdateCodes.java
@@ -0,0 +1,17 @@
+package de.hsh.Examples;
+
+import common.updates.UpdateCodes;
+
+public class ExampleUpdateCodes {
+
+    public class ErsteGui{
+        public static final String code1 = "code1";
+        public static final String code2 = "code2";
+    }
+
+    public class ZweiteGui{
+        public static final String code1 = "code1";
+        public static final String code2 = "code2";
+    }
+
+}
