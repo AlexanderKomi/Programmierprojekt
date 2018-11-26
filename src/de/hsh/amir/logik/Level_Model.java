b8d615f63e770e8962b4be71628367b47f4b49d9
piano337 <piano337@yahoo.de>
2018-11-26 16:27:11 +0100

    counter attribute created for counting points in levels

--- src/de/hsh/amir/logik/Level_Model.java
+++ src/de/hsh/amir/logik/Level_Model.java
@@ -5,14 +5,15 @@ import java.util.Observer;
 
 public class Level_Model extends Observable implements Observer {
 
-    public static final int LEVELMODEL_WIDTH  = 1280;
+    public static final int LEVELMODEL_WIDTH = 1280;
     public static final int LEVELMODEL_HEIGHT = 800;
+
 
     private Spielfigur_Model mSpielfigur;
 
     private int mMoveDir = 0;
 
-    public Level_Model(){
+    public Level_Model() {
         mSpielfigur = new Spielfigur_Model();
         mSpielfigur.addObserver(this);
     }
--- src/de/hsh/amir/logik/Spielfigur_Model.java
+++ src/de/hsh/amir/logik/Spielfigur_Model.java
@@ -7,14 +7,16 @@ public class Spielfigur_Model extends Observable {
     private int x;
     private int y; //Muss später final gesetzt werden, da sich Spielfigur nur nach links unr rechts bewegen kann.
 
-    public void move(final char direction_Char){
+    public static int counter = 0; //um Punkte zu zählen.
+
+    public void move(final char direction_Char) {
         switch (direction_Char) {
             case 'R': // nach rechts bewegen
-                x = Math.min(Level_Model.LEVELMODEL_WIDTH-100, x +5);
+                x = Math.min(Level_Model.LEVELMODEL_WIDTH - 100, x + 5);
                 setChanged();
                 break;
             case 'L': // nach links bewegen
-                x = Math.max(0, x -5);
+                x = Math.max(0, x - 5);
                 setChanged();
                 break;
         }
@@ -28,7 +30,7 @@ public class Spielfigur_Model extends Observable {
     }
 
     public void setX(int x) {
-        if (x!=this.x){
+        if (x != this.x) {
             this.x = x;
             setChanged();
         }
@@ -40,7 +42,7 @@ public class Spielfigur_Model extends Observable {
     }
 
     public void setY(int y) {
-        if (y!=this.y){
+        if (y != this.y) {
             this.y = y;
             setChanged();
         }
