ffba4b57b0068b8e205bc165a74af0970095ad61
Kevin Jeske <jeske.kevin@web.de>
2018-11-26 18:42:24 +0100

    Fixed BoundChecks

--- src/common/actor/BoundsChecks.java
+++ src/common/actor/BoundsChecks.java
@@ -10,82 +10,77 @@ import common.util.Logger;
  */
 class BoundsChecks {
 
-    static boolean doesCollide( Actor a, Actor b ) {
-        // TODO : Implementiere check 1 bis 5 und teste das.
-        return BoundsChecks.check1( a, b ) ||
-               BoundsChecks.check2( a, b ) ||
-               BoundsChecks.check3( a, b ) ||
-               BoundsChecks.check4( a, b ) ||
-               BoundsChecks.check5( a, b ) ||
-               BoundsChecks.check6( a, b )
-                ;
+    static boolean doesCollide(Actor a, Actor b) {
+	return checkUpperLeftCorner(a, b) || checkUpperRightCorner(a, b) || checkLowerRightCorner(a, b)
+		|| checkLowerLeftCorner(a, b);
     }
 
     /**
-     * Untere rechte Ecke von a, schneidet obere Linke Ecke von b.
+     * Obere Linke Ecke von b schneidet a
+     * 
+     * @param a
+     * @param b
+     * @return
      */
-    private static boolean check1( Actor a, Actor b ) {
-        if ( a.getX() < b.getX() ) {
-            if ( a.getX() + a.width > b.getX() ) {
-                if ( a.getX() < b.getY() ) {
-                    if ( a.getX() + a.height > b.getY() ) {
-                        Logger.log( "Check 1" );
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
+    private static boolean checkUpperLeftCorner(Actor a, Actor b) {
+	if (a.getX() <= b.getX() && a.getX() + a.width >= b.getX()) {
+	    if (a.getY() <= b.getY() && a.getY() + a.height >= b.getY()) {
+		Logger.log("Check Upper Left Corner");
+		return true;
+	    }
+	}
+	return false;
     }
 
     /**
-     * Untere linke Ecke von a, schneidet obere rechte Ecke von b.
+     * Obere Rechte Ecke von b schneidet a
+     * 
+     * @param a
+     * @param b
+     * @return
      */
-    private static boolean check2( Actor a, Actor b ) {
-        if ( a.getX() < b.getX() + b.width ) {
-            if ( a.getY() + a.height > b.getY() ) {
-
-                Logger.log( "Check 2" );
-                Logger.log( a );
-                Logger.log( b );
-                Logger.log( "\n" );
-
-                return true;
-            }
-        }
-        return false;
+    private static boolean checkUpperRightCorner(Actor a, Actor b) {
+	if (a.getX() <= b.getX() + b.width && a.getX() + a.width >= b.getX() + b.width) {
+	    if (a.getY() <= b.getY() && a.getY() + a.height >= b.getY()) {
+		Logger.log("Check Upper Right Corner");
+		return true;
+	    }
+	}
+	return false;
     }
 
     /**
-     * b schneidet a mittig von links.
+     * Untere Linke Ecke von b schneidet a
+     * 
+     * @param a
+     * @param b
+     * @return
      */
-    private static boolean check3( Actor a, Actor b ) {
-        if ( a.getX() < b.getX() + b.width ) {
-            if ( a.getY() + a.height > b.getX() + b.width ) {
-                Logger.log( "Check 3" );
-                return true;
-            }
-        }
-        return false;
+    private static boolean checkLowerLeftCorner(Actor a, Actor b) {
+	if (a.getX() <= b.getX() && a.getX() + a.width >= b.getX()) {
+	    if (a.getY() <= b.getY() + b.height && a.getY() + a.height >= b.getY() + b.height) {
+		Logger.log("Check Lower Left Corner");
+		return true;
+	    }
+	}
+	return false;
     }
 
-
-    private static boolean check4( Actor a, Actor b ) {
-        if ( a.getX() < b.getX() + b.width ) {
-            if ( a.getY() < b.getX() + b.height + b.width ) {
-                Logger.log( "Check 4" );
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean check5( Actor a, Actor b ) {
-        return false;
-    }
-
-    private static boolean check6( Actor a, Actor b ) {
-        return false;
+    /**
+     * Untere Rechte Ecke von b schneidet a
+     * 
+     * @param a
+     * @param b
+     * @return
+     */
+    private static boolean checkLowerRightCorner(Actor a, Actor b) {
+	if (a.getX() <= b.getX() + b.width && a.getX() + a.width >= b.getX() + b.width) {
+	    if (a.getY() <= b.getY() + b.height && a.getY() + a.height >= b.getY() + b.height) {
+		Logger.log("Check Lower Right Corner");
+		return true;
+	    }
+	}
+	return false;
     }
 
 }
