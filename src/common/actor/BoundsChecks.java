6a2564817425de258bb26b175eac46e2c24577a0
Alex <alexander.komischke@stud.uni-hannover.de>
2018-11-30 14:26:10 +0100

    Reformatted BoundsChecks

--- src/common/actor/BoundsChecks.java
+++ src/common/actor/BoundsChecks.java
@@ -1,7 +1,5 @@
 package common.actor;
 
-import common.util.Logger;
-
 /**
  * Bounds checking for collision.
  *
@@ -10,73 +8,69 @@ import common.util.Logger;
  */
 class BoundsChecks {
 
-    static boolean doesCollide(Actor a, Actor b) {
-	return checkUpperLeftCorner(a, b) || checkUpperRightCorner(a, b) || checkLowerRightCorner(a, b)
-		|| checkLowerLeftCorner(a, b);
+    static boolean doesCollide( Actor a, Actor b ) {
+        return checkUpperLeftCorner( a, b ) || checkUpperRightCorner( a, b ) || checkLowerRightCorner( a, b )
+               || checkLowerLeftCorner( a, b );
     }
 
     /**
      * Obere Linke Ecke von b schneidet a
-     * 
+     *
      * @param a
      * @param b
+     *
      * @return
      */
-    private static boolean checkUpperLeftCorner(Actor a, Actor b) {
-	if (a.getX() <= b.getX() && a.getX() + a.width >= b.getX()) {
-	    if (a.getY() <= b.getY() && a.getY() + a.height >= b.getY()) {
-		return true;
-	    }
-	}
-	return false;
+    private static boolean checkUpperLeftCorner( Actor a, Actor b ) {
+        if ( a.getX() <= b.getX() && a.getX() + a.width >= b.getX() ) {
+            return a.getY() <= b.getY() && a.getY() + a.height >= b.getY();
+        }
+        return false;
     }
 
     /**
      * Obere Rechte Ecke von b schneidet a
-     * 
+     *
      * @param a
      * @param b
+     *
      * @return
      */
-    private static boolean checkUpperRightCorner(Actor a, Actor b) {
-	if (a.getX() <= b.getX() + b.width && a.getX() + a.width >= b.getX() + b.width) {
-	    if (a.getY() <= b.getY() && a.getY() + a.height >= b.getY()) {
-		return true;
-	    }
-	}
-	return false;
-    }
-
-    /**
-     * Untere Linke Ecke von b schneidet a
-     * 
-     * @param a
-     * @param b
-     * @return
-     */
-    private static boolean checkLowerLeftCorner(Actor a, Actor b) {
-	if (a.getX() <= b.getX() && a.getX() + a.width >= b.getX()) {
-	    if (a.getY() <= b.getY() + b.height && a.getY() + a.height >= b.getY() + b.height) {
-		return true;
-	    }
-	}
-	return false;
+    private static boolean checkUpperRightCorner( Actor a, Actor b ) {
+        if ( a.getX() <= b.getX() + b.width && a.getX() + a.width >= b.getX() + b.width ) {
+            return a.getY() <= b.getY() && a.getY() + a.height >= b.getY();
+        }
+        return false;
     }
 
     /**
      * Untere Rechte Ecke von b schneidet a
-     * 
+     *
      * @param a
      * @param b
+     *
      * @return
      */
-    private static boolean checkLowerRightCorner(Actor a, Actor b) {
-	if (a.getX() <= b.getX() + b.width && a.getX() + a.width >= b.getX() + b.width) {
-	    if (a.getY() <= b.getY() + b.height && a.getY() + a.height >= b.getY() + b.height) {
-		return true;
-	    }
-	}
-	return false;
+    private static boolean checkLowerRightCorner( Actor a, Actor b ) {
+        if ( a.getX() <= b.getX() + b.width && a.getX() + a.width >= b.getX() + b.width ) {
+            return a.getY() <= b.getY() + b.height && a.getY() + a.height >= b.getY() + b.height;
+        }
+        return false;
+    }
+
+    /**
+     * Untere Linke Ecke von b schneidet a
+     *
+     * @param a
+     * @param b
+     *
+     * @return
+     */
+    private static boolean checkLowerLeftCorner( Actor a, Actor b ) {
+        if ( a.getX() <= b.getX() && a.getX() + a.width >= b.getX() ) {
+            return a.getY() <= b.getY() + b.height && a.getY() + a.height >= b.getY() + b.height;
+        }
+        return false;
     }
 
 }
