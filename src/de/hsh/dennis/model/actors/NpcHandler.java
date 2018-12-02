21d92ee1da4875ae1098a0b47b28271379a0289b
Dennis Sellemann <dsellemann@gmail.com>
2018-12-02 15:10:20 +0100

    thread probleme mit der Npc liste gefixt über synchronized

--- src/de/hsh/dennis/model/actors/NpcHandler.java
+++ src/de/hsh/dennis/model/actors/NpcHandler.java
@@ -11,7 +11,7 @@ public class NpcHandler {
     private int npcLimit = 100;
 
     private static Canvas canvas;
-    private static List<Npc> npcList = Collections.synchronizedList(new ArrayList());
+    private static final List<Npc> npcList = Collections.synchronizedList(new ArrayList());
     private List<Npc> npcsToRemove = Collections.synchronizedList(new ArrayList());
 
 
@@ -35,22 +35,26 @@ public class NpcHandler {
     }
 
     public static void drawNpcs() {
-        for (Npc npc : npcList) {
-            canvas.getGraphicsContext2D().drawImage(npc.getImage(), npc.getPosX(), npc.getPosY());
+        synchronized (npcList) {
+            for (Npc npc : npcList) {
+                canvas.getGraphicsContext2D().drawImage(npc.getImage(), npc.getPosX(), npc.getPosY());
+            }
         }
     }
 
     public void move() {
-        for (Npc npc : npcList) {
-            if (npc.getPosX() >= ((canvas.getWidth() / 2.0) - (npc.getImage().getWidth())) && (npc.getPosX() <= (canvas.getWidth() / 2.0))) {
-                npcsToRemove.add(npc);
-            } else {
-                npc.move();
+        synchronized (npcList) {
+            for (Npc npc : npcList) {
+                if (npc.getPosX() >= ((canvas.getWidth() / 2.0) - (npc.getImage().getWidth())) && (npc.getPosX() <= (canvas.getWidth() / 2.0))) {
+                    npcsToRemove.add(npc);
+                } else {
+                    npc.move();
+                }
             }
+            for (Npc npc : npcsToRemove) {
+                npcList.remove(npc);
+            }
+            npcsToRemove.clear();
         }
-        for (Npc npc : npcsToRemove) {
-            npcList.remove(npc);
-        }
-        npcsToRemove.clear();
     }
 }
