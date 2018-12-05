f206be761ae58d7c65fd632c6dbd0d8cb663c3c6
piano337 <piano337@yahoo.de>
2018-12-05 19:47:13 +0100

    just added two fields to AmirsGame

--- src/de/hsh/amir/logik/AmirGame.java
+++ src/de/hsh/amir/logik/AmirGame.java
@@ -4,9 +4,11 @@ import common.util.Logger;
 import javafx.scene.canvas.Canvas;
 
 public class AmirGame {
+    private Spielfigur spielfigur;
+    private Gegner gegner;
 
-    public void render( Canvas canvas, int fps ) {
-        Logger.log( "render" );
+    public void render(Canvas canvas, int fps) {
+        Logger.log("render");
     }
 
 }
