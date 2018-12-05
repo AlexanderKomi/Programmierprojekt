067ecacb00e67ca97bd05a8709d5c83beb87507e
Alex <alexander.komischke@stud.uni-hannover.de>
2018-12-05 14:52:17 +0100

    Added an extra game class for RAM

--- src/de/hsh/daniel/controller/RamGame_controller.java
+++ src/de/hsh/daniel/controller/RamGame_controller.java
@@ -1,6 +1,7 @@
 package de.hsh.daniel.controller;
 
 import common.util.Logger;
+import de.hsh.daniel.model.Game;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
@@ -9,16 +10,17 @@ import java.net.URL;
 import java.util.Observable;
 import java.util.ResourceBundle;
 
-/** This is the game started */
+/** This is the game fxml-controller started */
 public class RamGame_controller extends Observable implements Initializable {
 
     public static final String fxml = "view/RAMGame.fxml";
-
+    private             Game   game = new Game();
     @FXML
-    private Canvas gameCanvas;
+    private             Canvas gameCanvas;
 
     public void render( int fps ) {
         //Logger.log( "render" );
+        game.render( gameCanvas, fps );
     }
 
     @Override
