3b2a81d8bc90e6ce43423de963d29ed9cad85086
piano337 <piano337@yahoo.de>
2018-12-05 17:25:58 +0100

    Created new subclass GameField

---
+++ src/de/hsh/amir/logik/GameField.java
@@ -0,0 +1,29 @@
+package de.hsh.amir.logik;
+
+
+import common.actor.Level;
+import javafx.scene.input.KeyEvent;
+
+import java.io.FileNotFoundException;
+import java.util.Observable;
+
+public class GameField extends Level{
+
+    public GameField() throws FileNotFoundException {
+    }
+
+    @Override
+    public void createLevel() throws FileNotFoundException {
+
+    }
+
+    @Override
+    public void keyboardInput(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
