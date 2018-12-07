3e6668b01383c9eae762e64949c6378a92142040
piano337 <piano337@yahoo.de>
2018-12-07 22:46:54 +0100

    fxml corrected again and modified GegnerManager

--- src/de/hsh/amir/logic/GegnerManager.java
+++ src/de/hsh/amir/logic/GegnerManager.java
@@ -5,4 +5,13 @@ import java.util.ArrayList;
 public class GegnerManager {
     private ArrayList<Gegner> gegnerListe;
     private Points points;
+
+    public GegnerManager(ArrayList<Gegner> gegnerListe, Points points) {
+        this.gegnerListe = gegnerListe;
+        this.points = points;
+    }
+
+    public ArrayList<Gegner> getGegnerListe(){
+        return gegnerListe;
+    }
 }
