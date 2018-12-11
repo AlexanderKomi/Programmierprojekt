c8751dedcad5e0162a9b4bb98dabed45adab2695
piano337 <piano337@yahoo.de>
2018-12-11 14:33:21 +0100

    spiel korrigiert

--- src/de/hsh/amir/logic/AmirGame.java
+++ src/de/hsh/amir/logic/AmirGame.java
@@ -1,14 +1,24 @@
 package de.hsh.amir.logic;
 
+import common.actor.Direction;
 import common.util.Logger;
 import javafx.scene.canvas.Canvas;
+
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 
 public class AmirGame {
     private Spielfigur spielfigur;
     private Gegner gegner;
 
+    public void initializePlayer(Canvas canvas) throws FileNotFoundException {
+        HashMap<String, Direction> playerKeyMap = new HashMap<>();
+        playerKeyMap.put("Right", Direction.Right);
+        playerKeyMap.put("Left", Direction.Left);
+        spielfigur = new Spielfigur("de/hsh/amir/resources/playerFigur.png", playerKeyMap);
+    }
+
     public void render(Canvas canvas, int fps) {
-        spielfigur.draw(canvas);
         Logger.log("render");
     }
 
