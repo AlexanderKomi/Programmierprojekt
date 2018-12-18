353f6e8bd7208de17583a73de912623eb7af172a
Alex <alexander.komischke@stud.uni-hannover.de>
2018-12-18 14:09:58 +0100

    Fixed memory leaking

--- src/common/util/PlaySound.java
+++ src/common/util/PlaySound.java
@@ -9,11 +9,12 @@ public class PlaySound {
 
     private static String      musicFile = null;
     private static MediaPlayer mediaPlayer;
+    private static Media       sound;
 
     public static void playSound(String path){
         if(musicFile==null||!musicFile.equals(path)) { //Perfomance-Kniff
             musicFile = path;
-            Media sound = new Media(new File(musicFile).toURI().toString());
+            sound = new Media( new File( musicFile ).toURI().toString() );
             mediaPlayer = new MediaPlayer(sound);
         }
         mediaPlayer.play();
