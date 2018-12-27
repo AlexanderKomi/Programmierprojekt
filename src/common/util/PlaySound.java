659d1530eb71f160e1678ee0aa31c00e47ac33cb
Alex <alexander.komischke@stud.uni-hannover.de>
2018-12-27 18:56:54 +0100

    Made PlaySound ready for a Thread implementation

--- src/common/util/PlaySound.java
+++ src/common/util/PlaySound.java
@@ -9,54 +9,71 @@ import javafx.util.Duration;
 
 public final class PlaySound {
 
-    private static String      musicFile    = null;
-    private static Media       currentMedia = null;
-    private static MediaPlayer prop_mediaPlayer;
+    private static Player p = new Player();
 
     public static void playSound( final String path ) {
-        playSound( path, false );
+        p.playSound( path, false );
     }
 
     public static void playAndResetSound( final String path ) {
-        playSound( path, true );
+        p.playSound( path, true );
     }
 
     public static void playSound( final String path, final boolean resetMediaPlayer ) {
-        if ( musicFile == null || !musicFile.equals( path ) ) { //Perfomance-Kniff
-            try {
-                if ( prop_mediaPlayer != null ) {
-                    prop_mediaPlayer.dispose();
+        p.playSound( path, resetMediaPlayer );
+    }
+
+    static class Player implements Runnable {
+        private String      musicFile    = null;
+        private Media       currentMedia = null;
+        private MediaPlayer prop_mediaPlayer;
+        private boolean     resetTimer;
+
+        public void playSound( final String path, final boolean resetMediaPlayer ) {
+            resetTimer = resetMediaPlayer;
+            if ( musicFile == null || !musicFile.equals( path ) ) { //Perfomance-Kniff
+                try {
+                    if ( prop_mediaPlayer != null ) {
+                        prop_mediaPlayer.dispose();
+                    }
+                    musicFile = path;
+                    currentMedia = AudioBuffer.loadMedia( path );
+                    prop_mediaPlayer = createMediaPlayer();
                 }
-                musicFile = path;
-                currentMedia = AudioBuffer.loadMedia( path );
-                prop_mediaPlayer = createMediaPlayer();
+                catch ( MediaException me ) {
+                    me.printStackTrace();
+                }
             }
-            catch ( MediaException me ) {
-                me.printStackTrace();
+            if ( resetMediaPlayer ) {
+                resetTimer( prop_mediaPlayer );
+            }
+            Platform.runLater( () -> prop_mediaPlayer.play() );
+        }
+
+        private void resetTimer( MediaPlayer mediaPlayer ) {
+            if ( mediaPlayer.getCurrentTime().lessThan( mediaPlayer.getTotalDuration() ) ) {
+                mediaPlayer.stop();
+                mediaPlayer.seek( Duration.ZERO );
             }
         }
-        if ( resetMediaPlayer ) {
-            resetTimer( prop_mediaPlayer );
+
+        private MediaPlayer createMediaPlayer() {
+            MediaPlayer mediaPlayer = new MediaPlayer( currentMedia );
+            mediaPlayer.setAutoPlay( false );
+            mediaPlayer.setOnEndOfMedia( () -> {
+                mediaPlayer.stop();
+                mediaPlayer.seek( Duration.ZERO );
+                //mediaPlayer.dispose();
+            } );
+            return mediaPlayer;
         }
-        Platform.runLater( () -> prop_mediaPlayer.play() );
+
+        @Override
+        public void run() {
+            playSound( musicFile, resetTimer );
+        }
     }
 
-    private static void resetTimer( MediaPlayer mediaPlayer ) {
-        if ( mediaPlayer.getCurrentTime().lessThan( mediaPlayer.getTotalDuration() ) ) {
-            mediaPlayer.stop();
-            mediaPlayer.seek( Duration.ZERO );
-        }
-    }
 
-    private static MediaPlayer createMediaPlayer() {
-        MediaPlayer mediaPlayer = new MediaPlayer( currentMedia );
-        mediaPlayer.setAutoPlay( false );
-        mediaPlayer.setOnEndOfMedia( () -> {
-            mediaPlayer.stop();
-            mediaPlayer.seek( Duration.ZERO );
-            //mediaPlayer.dispose();
-        } );
-        return mediaPlayer;
-    }
 
 }
